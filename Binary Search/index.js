dd6d96577f61f933d7b91ec9f2e4ea5e3331dd18
Ashish Sharma <attach4ashish@gmail.com>
2020-05-02 23:53:42 +0530

    binary search: not completed just started

---
+++ Binary Search/index.js
@@ -0,0 +1,10 @@
+function searchElementIndex(arr, n) {
+    // [1,2,3,4,5,6,7,8,9,10], 3
+    let mediumIndex = Math.floor(arr.length / 2);
+    if (arr[mediumIndex] === n) {
+        return mediumIndex;
+    }
+    else if (arr[mediumIndex] > n) {
+
+    }
+}
