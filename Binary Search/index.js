d012e02de711a39591f315fe487e2510499693bc
Ashish Sharma <ashish@isummation.com>
2020-05-03 22:25:17 +0530

    binary search complete

--- Binary Search/index.js
+++ Binary Search/index.js
@@ -1,10 +1,22 @@
-function searchElementIndex(arr, n) {
-    // [1,2,3,4,5,6,7,8,9,10], 3
-    let mediumIndex = Math.floor(arr.length / 2);
-    if (arr[mediumIndex] === n) {
-        return mediumIndex;
-    }
-    else if (arr[mediumIndex] > n) {
+// ToDo: do it again....problem on the first time
 
+function binary_Search(items, value) {
+    var firstIndex = 0,
+        lastIndex = items.length - 1,
+        middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+
+    while (items[middleIndex] != value && firstIndex < lastIndex) {
+        if (value < items[middleIndex]) {
+            lastIndex = middleIndex - 1;
+        }
+        else if (value > items[middleIndex]) {
+            firstIndex = middleIndex + 1;
+        }
+        middleIndex = Math.floor((lastIndex + firstIndex) / 2);
     }
+
+    return (items[middleIndex] != value) ? -1 : middleIndex;
 }
+var items = [1, 2, 3, 4, 5, 7, 8, 9];
+console.log(binary_Search(items, 1));
+console.log(binary_Search(items, 5));
