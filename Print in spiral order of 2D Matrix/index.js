a9e9e73ac76e7df4391c6ddde0bbe94861e7747a
Ashish Sharma <ashish@isummation.com>
2020-05-06 06:21:14 +0530

    removed comment

--- Print in spiral order of 2D Matrix/index.js
+++ Print in spiral order of 2D Matrix/index.js
@@ -2,21 +2,16 @@
 
 
 function printSpiral(arr) {
-    let counter = 1;
     let startCol = 0;
     let endCol = arr.length - 1;
     let startRow = 0;
     let endRow = arr[0].length - 1;
     while (startCol <= endCol && startRow <= endRow) {
         for (let i = startCol; i <= endCol; i++) {
-            // result[startRow][i] = counter;
-            // counter++;
             console.log(arr[startRow][i]);
         }
         startRow++;
         for (let j = startRow; j <= endRow; j++) {
-            // result[j][endCol] = counter;
-            // counter++;
             console.log(arr[j][endCol]);
 
         }
@@ -24,15 +19,11 @@ function printSpiral(arr) {
         endCol--;
 
         for (let i = endCol; i >= startCol; i--) {
-            // result[endRow][i] = counter;
-            // counter++;
             console.log(arr[endRow][i]);
         }
 
         endRow--;
         for (let i = endRow; i >= startRow; i--) {
-            // result[i][startCol] = counter;
-            // counter++;
             console.log(arr[i][startCol]);
         }
 
