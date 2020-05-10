1d9f5fe5df6ad60faf9c56a61c6fc556b4077d2c
Ashish Sharma <ashish@isummation.com>
2020-05-10 22:50:51 +0530

    TODO: Find duplicates in O(n) time and O(1) extra space

---
+++ Find duplicates in O(n) time and O(1) extra space/index.js
@@ -0,0 +1,15 @@
+// Find duplicates in O(n) time and O(1) extra space | Set 1
+// Given an array of n elements which contains elements from 0 to n-1, with any of these numbers appearing any number of times. Find these repeating numbers in O(n) and using only constant memory space.
+// Example:
+
+// Input : n = 7 and array[] = {1, 2, 3, 1, 3, 6, 6}
+// Output: 1, 3, 6
+
+// Explanation: The numbers 1 , 3 and 6 appears more
+// than once in the array.
+
+// Input : n = 5 and array[] = {1, 2, 3, 4 ,3}
+// Output: 3
+
+// Explanation: The number 3 appears more than once
+// in the array.
