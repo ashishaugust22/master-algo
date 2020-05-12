47f203b195ae37c981be71eccaf7c7d4c5d0b867
Ashish Sharma <attach4ashish@gmail.com>
2020-05-12 19:04:10 +0530

    // 26. Remove Duplicates from Sorted Array

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
-var isPalindrome = function (x) {
-    if (x < 0) return false;
-    else if (x < 10) return true;
-
-    let arr = [];
-    let input = x;
-
-    while (input > 0) {
-        arr.push(input % 10);
-        input = Math.floor(input / 10);
-    }
-
-    const mid = Math.floor(arr.length / 2);
-    if (arr.length === 1) {
-        if (arr[0] !== arr[1]) {
-            return false;
-        } else {
-            return false;
+var removeDuplicates = function (nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+            i--;
         }
-    } else {
-        const pivot = arr.length % 2 === 0 ? -1 : 0;
-        for (let i = 0; i <= mid; i++) {
-            if (i === 0 && pivot === -1) { }
-            else {
-                if (arr[mid - i] !== arr[mid + i + pivot]) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
+    return nums.length;
 };
 
-
-let result = isPalindrome(10);
+let input = [1, 2, 2, 2, 3, 4, 4, 5];
+let result = removeDuplicates(input);
 
 console.log(result);
