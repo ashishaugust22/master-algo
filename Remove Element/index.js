c0eead7f4816299f030a2a0ab9d20c637d94508c
Ashish Sharma <ashish@isummation.com>
2020-05-12 19:19:58 +0530

    // 27. Remove Element

---
+++ Remove Element/index.js
@@ -0,0 +1,41 @@
+// 27. Remove Element
+// Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+// Example 1:
+
+// Given nums = [3,2,2,3], val = 3,
+
+// Your function should return length = 2, with the first two elements of nums being 2.
+
+// It doesn't matter what you leave beyond the returned length.
+// Example 2:
+
+// Given nums = [0,1,2,2,3,0,4,2], val = 2,
+
+// Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+
+// Note that the order of those five elements can be arbitrary.
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return nums.length;
+};
+
+let input = [1, 2, 2, 3, 4];
+let result = removeElement(input, 2);
+
+console.log(result);
