54036bc43fbeded2e8b39517bc6d527610f7b150
Ashish Sharma <attach4ashish@gmail.com>
2020-05-12 19:19:58 +0530

    // 27. Remove Element

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var removeDuplicates = function (nums) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] === nums[i + 1]) {
+var removeElement = function (nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
             nums.splice(i, 1);
             i--;
         }
@@ -8,7 +8,7 @@ var removeDuplicates = function (nums) {
     return nums.length;
 };
 
-let input = [1, 2, 2, 2, 3, 4, 4, 5];
-let result = removeDuplicates(input);
+let input = [1, 2, 2, 3, 4];
+let result = removeElement(input, 2);
 
 console.log(result);
