a780de4856d54aa7dc2a4483077c7575b2ec2f31
Ashish Sharma <ashish@isummation.com>
2020-05-17 10:49:47 +0530

    Merge Sorted Array 2nd approch

--- Merge Sorted Array/index.js
+++ Merge Sorted Array/index.js
@@ -1,3 +1,21 @@
+// Merge Sorted Array
+
+
+// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+// Note:
+
+// The number of elements initialized in nums1 and nums2 are m and n respectively.
+// You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+// Example:
+
+// Input:
+// nums1 = [1,2,3,0,0,0], m = 3
+// nums2 = [2,5,6],       n = 3
+
+// Output: [1,2,2,3,5,6]
+
+
 /**
  * @param {number[]} nums1
  * @param {number} m
@@ -6,7 +24,6 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 
-// TODO: do it again
 var merge = function (nums1, m, nums2, n) {
     let i = m - 1;
     let j = n - 1;
@@ -34,10 +51,46 @@ var merge = function (nums1, m, nums2, n) {
             j--; current--;
         }
     }
+    return nums1;
 };
 
 let result = merge([1, 2, 3, 0, 0, 0], 6, [2, 5, 6], 3);
 console.log(result);
+
+
+
+
+
+// 2nd Approch
+
+var merge = function (nums1, m, nums2, n) {
+    m--;
+    n--;
+    let index = nums1.length - 1;
+    debugger;
+    while (index >= 0) {
+        if (m < 0) {
+            nums1[index] = nums2[n];
+            n--;
+        } else if (n < 0) {
+            nums1[index] = nums1[m];
+            m--;
+        } else {
+            if (nums1[m] > nums2[n]) {
+                nums1[index] = nums1[m];
+                m--;
+            } else {
+                nums1[index] = nums2[n];
+                n--;
+            }
+        }
+        index--;
+    }
+    console.log(nums1);
+};
+
+let result = merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
+
 
 // Input:
 // nums1 = [1,2,3,0,0,0], m = 3
