9ed1025827cc9629fb8abcba8541c3e09a6d94a1
Ashish Sharma <ashishsharma@MacBook-Air.local>
2020-06-09 09:46:09 +0530

    heapSort implementation

---
+++ heapsort/index.js
@@ -0,0 +1,38 @@
+let heapLen;
+let heapSort = function(arr){
+	heapLen = arr.length;
+
+	for(let i = Math.floor(arr.length/2); i>=0; i--){
+		heapify(arr,i);
+	}
+
+	for(let i = arr.length - 1; i > 0; i--){
+		swap(arr,0,i);
+		heapLen--;
+		heapify(arr,0);
+	}
+	return arr;
+}
+
+let heapify = function(arr,i){
+	let parent = i;
+	let left = 2*i + 1;
+	let right = 2*i + 2;
+	
+	if(left < heapLen && arr[left]>arr[parent]){
+		parent = left;
+	}
+	if(right < heapLen && arr[right]>arr[parent]){
+		parent = right;
+	}
+	if( i !== parent){
+		swap(arr,parent,i);
+		heapify(arr,0);
+	}
+}
+
+let swap = function(arr,i,j){
+	let temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
