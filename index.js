5c086ec17ae442ffeecc84c12eb7ddbb10e69970
Ashish Sharma <ashish@isummation.com>
2020-07-26 18:15:12 +0530

    design ques: Desing a linked list data structure

--- index.js
+++ index.js
@@ -25,7 +25,54 @@ class LinkedList {
         this.size++;
     };
     insertAt = function (element, index) {
+        if (index > 0 && index > this.size) return false;
 
+        let node = new Node(element);
+        let curr_index = 0;
+
+        if (index === 0) {
+            node.next = this.head;
+            this.head = node;
+        } else {
+            let current = this.head;
+            while (curr_index === index) {
+                curr_index++;
+                previous = current;
+                current = current.next;
+            }
+            previous.next = node;
+            node.next = current;
+        }
+        this.size++;
     };
+
+    removeFrom = function (index) {
+        return 'not implemented';
+    };
+    removeElement = function (element) {
+        return 'not implemented';
+    };
+
+
+    //Helper Methods
+    indexOf = function (element) {
+        return 'not implemented';
+    };
+    isEmpty = function () {
+        return this.size === 0 ? true : false;
+    };
+    size_of_list = function () {
+        return this.size;
+    };
+    printList = function () {
+        var curr = this.head;
+        var str = "";
+        while (curr) {
+            str += curr.element + " ";
+            curr = curr.next;
+        }
+        console.log(str);
+    };
+
 }
 
