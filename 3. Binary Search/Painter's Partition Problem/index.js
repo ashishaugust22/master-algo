91811215a0b8d1362329b3f373300f9d9731c68e
Ashish Sharma <attach4ashish@gmail.com>
2020-12-27 17:22:08 +0530

    Painter's Partition Problem

---
+++ 3. Binary Search/Painter's Partition Problem/index.js
@@ -0,0 +1,121 @@
+// Painter's Partition Problem
+// Problem Description
+
+// Given 2 integers A and B and an array of integers C of size N. Element C[i] represents length of ith board.
+// You have to paint all N boards [C0, C1, C2, C3 … CN-1]. There are A painters available and each of them takes B units of time to paint 1 unit of board.
+
+// Calculate and return minimum time required to paint all boards under the constraints that any painter will only paint contiguous sections of board.
+// NOTE:
+// 1. 2 painters cannot share a board to paint. That is to say, a board cannot be painted partially by one painter, and partially by another.
+// 2. A painter will only paint contiguous boards. Which means a configuration where painter 1 paints board 1 and 3 but not 2 is invalid.
+
+// Return the ans % 10000003.
+
+
+
+// Problem Constraints
+// 1 <= A <= 1000
+// 1 <= B <= 106
+// 1 <= N <= 105
+// 1 <= C[i] <= 106
+
+
+
+// Input Format
+// The first argument given is the integer A.
+// The second argument given is the integer B.
+// The third argument given is the integer array C.
+
+
+
+// Output Format
+// Return minimum time required to paint all boards under the constraints that any painter will only paint contiguous sections of board % 10000003.
+
+
+
+// Example Input
+// Input 1:
+
+//  A = 2
+//  B = 5
+//  C = [1, 10]
+// Input 2:
+
+//  A = 10
+//  B = 1
+//  C = [1, 8, 11, 3]
+
+
+// Example Output
+// Output 1:
+
+//  50
+// Output 2:
+
+//  11
+
+
+// Example Explanation
+// Explanation 1:
+
+//  Possibility 1:- One painter paints both blocks, time taken = 55 units.
+//  Possibility 2:- Painter 1 paints block 1, painter 2 paints block 2, time take = max(5, 50) = 50
+//  There are no other distinct ways to paint boards.
+//  ans = 50%10000003
+// Explanation 2:
+
+//  Each block is painted by a painter so, Painter 1 paints block 1, painter 2 paints block 2, painter 3 paints block 3 
+//  and painter 4 paints block 4, time taken = max(1, 8, 11, 3) = 11
+//  ans = 11%10000003
+
+
+
+
+
+module.exports = {
+    //param A : integer   painter
+    //param B : integer   unit time
+    //param C : array of integers
+    //return an integer
+    paint: function (A, B, C) {
+        let totalUnits = this.calcTotalUnits(C);
+        let maxTime = totalUnits * B;
+        let bestTime = maxTime;
+
+        let leastTime = Math.max(...C) * B;
+
+        while (maxTime - leastTime >= 0) {
+            let time = Math.floor(leastTime + (maxTime - leastTime) / 2);
+            let painters = this.getPaintersRequired(C, B, time);
+            if (painters <= A) {
+                bestTime = time;
+                maxTime = time - 1;
+            } else if (painters > A) {
+                leastTime = time + 1;
+            }
+        }
+
+        return bestTime % 10000003;
+    },
+    calcTotalUnits: function (C) {
+        let sum = 0;
+        for (let i = 0; i < C.length; i++) {
+            sum += C[i];
+        }
+        return sum;
+    },
+    getPaintersRequired: function (C, B, time) {
+        let count = 1;
+        let sum = 0;
+        for (let i = 0; i < C.length; i++) {
+            if (sum + C[i] * B <= time) {
+                sum += C[i] * B;
+            } else {
+                count++;
+                sum = C[i] * B;
+            }
+        }
+
+        return sum <= time ? count : Infinity;
+    }
+};
