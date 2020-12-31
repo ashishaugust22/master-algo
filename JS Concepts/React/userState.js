b6b0c66c621483842f689b4537250b5592655e93
Ashish Sharma <attach4ashish@gmail.com>
2020-12-31 19:44:49 +0530

    react hooks

--- JS Concepts/React/userState.js
+++ JS Concepts/React/userState.js
@@ -1,14 +1,3 @@
-// https://www.netlify.com/blog/2019/03/11/deep-dive-how-do-react-hooks-really-work/
-// https://medium.com/dailyjs/i-never-understood-javascript-closures-9663703368e8
-
-// Closure is when a function is able to remember and access its lexical scope even when 
-// that function is executing outside its lexical scope.
-
-// They’re obviously closely tied to the concept of lexical scoping, which MDN defines as 
-// “how a parser resolves variable names when functions are nested”. 
-// Let’s look at a practical example to better illustrate this:
-
-// Example 0
 function useState(initialValue) {
     var _val = initialValue; // _val is a local variable created by useState
     function state() {
