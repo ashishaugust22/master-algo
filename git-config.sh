770e135d1ba92e10f36eb237e08bd98471031eb0
Ashish Sharma <ashishaugust22@gmail.com>
2021-05-24 19:42:33 +0530

    git config file

---
+++ git-config.sh
@@ -0,0 +1,13 @@
+git config user.name "Ashish Sharma"
+
+git config user.email "ashishaugust22@gmail.com"
+
+cat .git/config
+
+
+# execute on mac using
+# sh ./git-config.sh
+
+
+# execute on windows using
+# ./git-config.sh
