299ee740407ac22aef5763850c7feea2371e1912
Ashish S <92317433+ashishaugust22@users.noreply.github.com>
2024-01-11 16:23:06 +0100

    binary search algo with recursion

--- src/3. Binary Search/index.js
+++ src/3. Binary Search/index.js
@@ -20,3 +20,28 @@ function binary_Search(items, value) {
 var items = [1, 2, 3, 4, 5, 7, 8, 9];
 console.log(binary_Search(items, 1));
 console.log(binary_Search(items, 5));
+
+
+function recursiveBinarySearch(items, value) {
+    const left = 0;
+    const right = items.length - 1;
+
+    const index = binarySearch(items, left, right, value);
+
+    return index;
+}
+
+function binarySearch(items, left, right, target) {
+    const middle = Math.floor((right + left) / 2);
+    if (items[middle] === target) return middle;
+
+    if (items[middle] < target) left = middle + 1;
+    if (items[middle] > target) right = middle - 1;
+
+    if (left > right) return -1;
+
+    return binarySearch(items, left, right, target);
+}
+
+// var items = [1, 2, 3, 4, 5, 7, 8, 9];
+console.log(recursiveBinarySearch(items, 1));
